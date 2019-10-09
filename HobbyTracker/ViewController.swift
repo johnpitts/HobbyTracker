ba23696947e1aa6128857d45152884bdc3adfa28
John Pitts <jgpitts@gmail.com>
2019-10-08 22:12:04 -0400

    Built layout for Hobby Tracker app including viewController with embedded TableView, a viewController to add hobbies, and a viewController to view details.  constrained all objects

---
+++ HobbyTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HobbyTracker
+//
+//  Created by John Pitts on 10/8/19.
+//  Copyright © 2019 johnpitts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
